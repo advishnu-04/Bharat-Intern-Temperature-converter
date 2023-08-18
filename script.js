3ffeab19e8460e6ba3e9da0945506164d103192a
BONI ADI VISHNU <adivishnu09@gmail.com>
2023-08-18 12:09:29 +0530

    Temperature Converter

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("convertBtn").addEventListener("click", function(){
+    var celsiusInput = document.getElementById("celsius");
+    var fahrenheitInput = document.getElementById("fahrenheit");
+
+    var celsius = parseFloat(celsiusInput.value);
+    var fahrenheit = (celsius * 9/5)+ 32;
+
+    fahrenheitInput.value = fahrenheit.toFixed(2);
+});
